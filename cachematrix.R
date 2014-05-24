7838fa76ac107af2b1e2cded1b99511a81c98db4
Ricks <pricardo03@users.noreply.github.com>
2014-05-24 14:57:38 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,11 +2,25 @@
 ## functions do
 
 ## Write a short comment describing this function
+# Spanish comment
+# makeCacheMatrix es un proceso mediante el cual voy a llenar con valores una matrix
+# que debe cumplir con los siguientes lineamientos:
+# a. debe ser cuadrada
+# b. debe contener valores numericos
+# 
+
+# english
+# makeCacheMatrix is a process where will fill a matrix with values and have some
+# specifications like:
+# a. have to be a square matrix
+# b. values have to be numerics 
 
 makeCacheMatrix <- function(x = matrix()) {
 #Tasks
 #1. create the matrix
 #2. fill the matrix
+
+
 
 }
 
