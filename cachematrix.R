55753ccc8c8c0db7a2ffbb525b331593356f3482
Ricks <pricardo03@users.noreply.github.com>
2014-05-25 17:08:10 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -13,38 +13,50 @@ makeCacheMatrix <- function(x = matrix())
         CachedMatrix <- NULL
         
         set <- function(y) 
-        {
-                x <<- y
-        }
+                { x <<- y }
         
         get <- function() x
         
+        set.inverse <- function(i) inv.cache <<- i
         get.inverse <- function() inv.cache
         
         ## Here we assign the value to the variable on top-level
-        CachedMatrix <<- cacheSolve(x)
+        #CachedMatrix <<- cacheSolve(x)
         
-        #list(set = set, get = get)
+        list(set = set, get = get)
 }
 
 
 ## Write a short comment describing this function
 ## this func() complete two steps
 ## 1. check if conversion is possible
-## 2. if possible make a inverse and return to the caller
-## If the inverse has already been calculated 
+## 2. if possible make a inverse and return to the caller If the inverse has already been calculated 
 cacheSolve <- function(x, ...) 
 {
         ## Return a matrix that is the inverse of 'x'
         
         # Try to get the inverse matrix from cache
-        inverse <- x$get.inverse()  
-
-        #1. Check if inverse of matrix "x" can be done
-        #   and matrix is not null
-        if(ncol(x)==nrow(x) && !is.null(x)              ##< only square matrix can be done 
-           && !identical(CachedMatrix, x))              ##< If the inverse has not already been calculated 
-                { return(solve(x)) }                    ##< returning the result of inverse
+        # and check if NOT exist the inverse in cache.If exist return it
+        if(!is.null(x$get.inverse())) 
+                { return(inv) }
         else 
-                { print("This matrix can inverse!") } #this create a warning on compile but explain what's happens
+                {
+                
+                        data <- x$get()
+                        x.set.inverse(solve(data))
+
+
+        data <- z$get()
+        inverse <- solve(data, ...)
+        z$set.inverse(inverse)
+
+                }
+        
+        #because NOT EXISTS bring the matrix
+        myData <- x$get()
+        
+        #1. Check if inverse of matrix "x" can be done
+        if(ncol(x)==nrow(x))                    ##< only square matrix can be done 
+                { x$set(solve(myData)) }        ##now set the inverse
+
 }
