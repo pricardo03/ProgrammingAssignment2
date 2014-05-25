c22228913e7a319400b838c5cefb75497e132fc1
Ricks <pricardo03@users.noreply.github.com>
2014-05-25 17:13:46 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -7,56 +7,48 @@
 # a. have to be a square matrix
 # b. values have to be numerics 
 
-makeCacheMatrix <- function(x = matrix()) 
-{
-        #1 create empty var for the inverse matrix 
-        CachedMatrix <- NULL
-        
-        set <- function(y) 
-                { x <<- y }
-        
-        get <- function() x
-        
-        set.inverse <- function(i) inv.cache <<- i
-        get.inverse <- function() inv.cache
-        
-        ## Here we assign the value to the variable on top-level
-        #CachedMatrix <<- cacheSolve(x)
-        
-        list(set = set, get = get)
+
+makeCacheMatrix <- function(x = matrix()) {
+    CacheForInverse <- NULL
+    #Inv.cache <- NULL
+
+    set <- function(y) {
+        # `<<-` sets the values of x and inv.cache in makeCacheMatrix's
+        # environment and not this function
+        x <<- y
+	CacheForInverse <<- NULL
+#        inv.cache <<- NULL
+    }
+
+    get <- function() x
+
+    SetInverse <- function(i) CacheForInverse <<- i
+    GetInverse <- function() CacheForInverse 
+
+#    set.inverse <- function(i) inv.cache <<- i
+#    get.inverse <- function() inv.cache
+   
+    return (list(set = set, get = get, SetInverse = SetInverse, GetInverse = GetInverse))
+
 }
 
 
-## Write a short comment describing this function
-## this func() complete two steps
-## 1. check if conversion is possible
-## 2. if possible make a inverse and return to the caller If the inverse has already been calculated 
-cacheSolve <- function(x, ...) 
-{
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) {
+    # Try make inverse from cache
+    inverse <- x$get.inverse()   
+    
+    # now check if the inverse exists
+	if(!is.null(inverse)) 
+	{
+		message("getting cached data")
+        	return(inverse)
+    	} 
+	else 
+	{
+        Data <- x$get()
+        inverse <- solve(Data, ...)
+        x$set.inverse(inverse)
         
-        # Try to get the inverse matrix from cache
-        # and check if NOT exist the inverse in cache.If exist return it
-        if(!is.null(x$get.inverse())) 
-                { return(inv) }
-        else 
-                {
-                
-                        data <- x$get()
-                        x.set.inverse(solve(data))
-
-
-        data <- z$get()
-        inverse <- solve(data, ...)
-        z$set.inverse(inverse)
-
-                }
-        
-        #because NOT EXISTS bring the matrix
-        myData <- x$get()
-        
-        #1. Check if inverse of matrix "x" can be done
-        if(ncol(x)==nrow(x))                    ##< only square matrix can be done 
-                { x$set(solve(myData)) }        ##now set the inverse
-
+        return(inverse)
+	}
 }
